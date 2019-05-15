1d3b1f6dcb89dd798af58b71706e9638a1562679
Bjoern Muellenmeister <b.muellenmeister@gmail.com>
2019-05-15 17:09:59 +0200

    created restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name:         'Ita-Food',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '2342352',
+    category:     'italian'
+  },
+  {
+    name:         'Bel-Food',
+    address:      '7 Bel St, Bruessels E2 7JE',
+    phone_number: '2345235',
+    category:     'belgian'
+  },
+  {
+    name:         'Jap-Food',
+    address:      '7 Jap St, Hong Kong E2 7JE',
+    phone_number: '235423562',
+    category:     'japanese'
+  },
+  {
+    name:         'French-food',
+    address:      '7 French St, Paris E2 7JE',
+    phone_number: '663453453',
+    category:     'french'
+  },
+  {
+    name:         'Chin-Food',
+    address:      '7 Chin St, Peking E2 7JE',
+    phone_number: '234235235',
+    category:     'chinese'
+  },
+]
+
+Restaurant.create!(restaurants_attributes)
